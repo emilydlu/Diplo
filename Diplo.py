3a2b16b4940c4f1db455a8743ad67331d7f7a532
Emily Lu <luem@bc.edu>
2016-12-04 16:06:21 -0500

    add twilio, clean stuff up

--- Diplo.py
+++ Diplo.py
@@ -1,24 +1,35 @@
 from bs4 import BeautifulSoup
 import urllib2
-import sched, time
-import random
+from apscheduler.schedulers.blocking import BlockingScheduler
+from twilio.rest import TwilioRestClient
+import sys
 
+account_sid = # Your Account SID from www.twilio.com/console
+auth_token  = # Your Auth Token from www.twilio.com/console
+client = TwilioRestClient(account_sid, auth_token)
 
-s = sched.scheduler(time.time, time.sleep)
+job = BlockingScheduler()
+
 def isDiploButtonLive():
     r = urllib2.urlopen('https://tickets.bijouboston.com/details/582331963ecd9a0d5762c761')
     soup = BeautifulSoup(r.read(), "html.parser")
     if "disabled" in str(soup.find_all(id="buy-tickets")[0]):
-        print 'false'
+        return False
     else:
-        print 'true'
-    sleep = random.randint(0, 5)
-    s.enter(sleep, 1, isDiploButtonLive, ())
+        return True
 
-s.enter(5, 1, isDiploButtonLive, ())
-s.run()
+@job.scheduled_job('cron', minute='10-20')
+def timed_checker():
+    phone_number = #Your phone number here
+    twilio_number = #Your twilio number here
+    if isDiploButtonLive():
+        message = client.messages.create(body="Diplo tickets are live!!! GO BUY",
+                                         to= phone_number, # Replace with your phone number
+                                         from_= twilio_number)  # Replace with your Twilio number
+    else:
+        message = client.messages.create(body="Diplo tickets are not :(",
+                                         to=phone_number,  # Replace with your phone number
+                                         from_=twilio_number)  # Replace with your Twilio number
 
+job.start()
 
-    # def periodicallyHit():
-
-# print isDiploButtonLive()
