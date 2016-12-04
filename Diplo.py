02863bd6e80fddb5a71a49e73b8cab3b23c92341
Emily Lu <luem@bc.edu>
2016-12-04 16:13:08 -0500

    get rid of error debugging

--- Diplo.py
+++ Diplo.py
@@ -4,13 +4,14 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from twilio.rest import TwilioRestClient
 import sys
 
-account_sid = # Your Account SID from www.twilio.com/console
-auth_token  = # Your Auth Token from www.twilio.com/console
+account_sid =  # Your Account SID from www.twilio.com/console
+auth_token  =  # Your Auth Token from www.twilio.com/console
 client = TwilioRestClient(account_sid, auth_token)
 
 job = BlockingScheduler()
 
 def isDiploButtonLive():
+    print "checking Diplo button"
     r = urllib2.urlopen('https://tickets.bijouboston.com/details/582331963ecd9a0d5762c761')
     soup = BeautifulSoup(r.read(), "html.parser")
     if "disabled" in str(soup.find_all(id="buy-tickets")[0]):
@@ -20,16 +21,12 @@ def isDiploButtonLive():
 
 @job.scheduled_job('cron', minute='10-20')
 def timed_checker():
-    phone_number = #Your phone number here
-    twilio_number = #Your twilio number here
+    phone_number = #your number
+    twilio_number = #your twilio number
     if isDiploButtonLive():
         message = client.messages.create(body="Diplo tickets are live!!! GO BUY",
                                          to= phone_number, # Replace with your phone number
                                          from_= twilio_number)  # Replace with your Twilio number
-    else:
-        message = client.messages.create(body="Diplo tickets are not :(",
-                                         to=phone_number,  # Replace with your phone number
-                                         from_=twilio_number)  # Replace with your Twilio number
 
 job.start()
 
